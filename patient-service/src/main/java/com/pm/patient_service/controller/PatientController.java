5095896c428d2e3a46f9497debed17c9978ce7b9
shivkantkaushik <shivkantkaushik31@gmail.com>
2025-07-05 23:08:50 +0530

    added controller mapper repo dto service, created flow for getting all patients

---
+++ patient-service/src/main/java/com/pm/patient_service/controller/PatientController.java
@@ -0,0 +1,28 @@
+package com.pm.patient_service.controller;
+
+import com.pm.patient_service.dto.PatientResponseDTO;
+import com.pm.patient_service.service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/patients")
+public class PatientController {
+
+    @Autowired
+    private PatientService patientService;
+
+
+    @GetMapping
+    public ResponseEntity<List<PatientResponseDTO>> getAllPatients(){
+
+        List<PatientResponseDTO> patients = patientService.getPatients();
+
+        return ResponseEntity.ok(patients);
+    }
+}
---
+++ patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
@@ -0,0 +1,32 @@
+package com.pm.patient_service.service;
+
+import com.pm.patient_service.dto.PatientResponseDTO;
+import com.pm.patient_service.mapper.PatientMapper;
+import com.pm.patient_service.model.Patient;
+import com.pm.patient_service.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PatientService {
+
+
+
+    @Autowired
+    PatientRepository patientRepository;
+
+
+    public List<PatientResponseDTO> getPatients(){
+
+        List<Patient> patients = patientRepository.findAll();
+
+        List<PatientResponseDTO> patientResponseDTOS = patients.stream().map(PatientMapper::toDTO).toList();
+
+        return patientResponseDTOS;
+
+
+    }
+
+}
